f4ea90b75b0e131776a0564ffdd6fd8ee3f94586
Muhammad Salman <muhammadsalman.webdev@gmail.com>
2024-09-27 18:14:59 +0500

    Video Gallery Issue

--- script.js
+++ script.js
@@ -215,4 +215,96 @@ allFlipCardsElems.forEach((flipCard) => {
   // Add click event listener to flip the card back
   toggleFrontBtn.addEventListener("click", flipToFront);
   popupOverlay.addEventListener("click", flipToFront);
+
+  // (((((((((((((((((((((((((((((((())))))))))))))))))))))))))))))))
+
+  const galleryBtns = flipCard.querySelectorAll(
+    "#customGallerySection .c-btns > button[data-card-label]"
+  );
+
+  const galleryCards = flipCard.querySelectorAll(
+    "#customGallerySection .custom_gallery-cards > .custom_gallery-card"
+  );
+
+  galleryBtns.forEach((btn, i) => {
+    if (btn.classList.contains("active")) {
+      const label = btn.getAttribute("data-card-label");
+      galleryCards.forEach((card) => {
+        const cLabel = card.getAttribute("data-card-label");
+        if (cLabel === label) {
+          card.classList.add("show");
+          card.classList.add("anim");
+        }
+      });
+    }
+
+    btn.addEventListener("click", () => {
+      galleryBtns.forEach((_b) => _b.classList.remove("active"));
+      btn.classList.add("active");
+      const label = btn.getAttribute("data-card-label");
+      galleryCards.forEach((card) => {
+        // Showing new cards
+        const cLabel = card.getAttribute("data-card-label");
+        if (label === "all") {
+          card.classList.add("show");
+          setTimeout(() => {
+            card.classList.add("anim");
+          }, 10);
+        } else {
+          if (cLabel === label) {
+            card.classList.add("show");
+            setTimeout(() => {
+              card.classList.add("anim");
+            }, 10);
+          } else {
+            // Hiding previous cards
+            card.classList.remove("anim");
+            setTimeout(() => {
+              card.classList.remove("show");
+            }, 600);
+          }
+        }
+      });
+    });
+  });
+
+  galleryCards.forEach((card, i) => {
+    const thumbnail = card.querySelector(".box"),
+      overLay = card.querySelector(".overlay"),
+      closeIcon = card.querySelector(".close-icon"),
+      playPauseBtn = card.querySelector(".play-pause-btn"),
+      video = card.querySelector("video");
+
+    thumbnail.addEventListener("click", () => {
+      card.querySelector(".popup").style.height = popup.scrollHeight + "px";
+      // popup.style.overflow = "hidden";
+      card.classList.add("show-popup");
+      setTimeout(() => {
+        card.classList.add("anim-popup");
+      }, 10);
+    });
+    const closeVideo = () => {
+      card.classList.remove("anim-popup");
+      setTimeout(() => {
+        card.classList.remove("show-popup");
+        popup.style.overflow = "auto";
+      }, 300);
+
+      if (playPauseBtn.classList.contains("played")) {
+        playPauseBtn.classList.remove("played");
+        video.pause();
+      }
+    };
+    overLay.addEventListener("click", closeVideo);
+    closeIcon.addEventListener("click", closeVideo);
+
+    playPauseBtn.addEventListener("click", (e) => {
+      playPauseBtn.classList.toggle("played");
+      if (playPauseBtn.classList.contains("played")) {
+        video.play();
+      } else {
+        video.pause();
+      }
+    });
+  });
 });
